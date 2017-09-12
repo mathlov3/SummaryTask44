e6cf57e561595c3cd431df93248a03cd01ae4380
Eugene <vzrivpaket22@gmail.com>
2017-09-13 00:08:38 +0300

    1 commit

---
+++ src/ua/nure/sliva/SummaryTask4/web/controller/ClearCart.java
@@ -0,0 +1,26 @@
+package ua.nure.sliva.SummaryTask4.web.controller;
+
+import ua.nure.sliva.SummaryTask4.Cart;
+import ua.nure.sliva.SummaryTask4.entity.Product;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/clearCart")
+public class ClearCart extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cart<Product> cart = (Cart<Product>) request.getSession().getAttribute("cart");
+        cart.clear();
+        response.sendRedirect("cart.jsp");
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req,resp);
+    }
+}
